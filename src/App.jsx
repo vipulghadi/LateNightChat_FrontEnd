82189057dbc73e4e7fdbd2a1c69efba2334567df
vipulghadi <vipulvijayghadi@gmail.com>
2024-12-26 23:31:42 +0530

    SETUP

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  
+
+  return (
+    <div className='text-3xl font-bold text-pink-500 bg-black  w-full h-screen flex justify-center items-center'> Welcome to the home page</div>
+  )
+}
+
+export default App
